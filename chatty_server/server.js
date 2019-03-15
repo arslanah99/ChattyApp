4b1d84274519764275aa1566d58ead3fa510c877
arslanah99 <Arah09@hotmail.com>
2019-03-15 16:26:23 +0000

    Completed project without linting

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,17 +1,19 @@
 // server.js
 
-const express = require('express');
-const SocketServer = require('ws').Server;
-const wsServer = require('ws').OPEN;
+const express = require("express");
+const SocketServer = require("ws").Server;
+const wsServer = require("ws").OPEN;
 
 // Set the port to 3001
 const PORT = 3001;
 
 // Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
-  .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+  // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static("public"))
+  .listen(PORT, "0.0.0.0", "localhost", () =>
+    console.log(`Listening on ${PORT}`)
+  );
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
@@ -19,34 +21,27 @@ const wss = new SocketServer({ server });
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
-wss.on('connection', (ws) => {
-
+wss.on("connection", ws => {
   const userCounter = {
     userCount: wss.clients.size,
-    type: 'UserCounter'
-  }
-  wss.clients.forEach(function each(client){
-    if(client.readyState === wsServer){
-      client.send(JSON.stringify(userCounter))
-
+    type: "UserCounter"
+  };
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === wsServer) {
+      client.send(JSON.stringify(userCounter));
     }
+  });
 
-  })
-  console.log('Client connected');
-  console.log(wss.clients.size)
 
-ws.on('message', function incoming(data){
-  // let stringData = JSON.stringify(data)
-  wss.clients.forEach(function each(client){
-    if(client.readyState === wsServer){
-      client.send(data)
-
-    }
-
-  })
- 
-})
+  ws.on("message", function incoming(data) {
+    // let stringData = JSON.stringify(data)
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === wsServer) {
+        client.send(data);
+      }
+    });
+  });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on("close", () => console.log("Client disconnected"));
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,112 +1,103 @@
-const uuidv1 = require('uuid/v1')
-import React, {Component} from 'react';
-import ChatBar from './ChatBar.jsx';
-import Message from './Message.jsx';
-import MessageList from './MessageList.jsx';
-import generateRandomId from './generateRandomId.js'
+const uuidv1 = require("uuid/v1");
+import React, { Component } from "react";
+import ChatBar from "./ChatBar.jsx";
+import MessageList from "./MessageList.jsx";
 
 class App extends Component {
-  
-
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.handleChange = this.handleChange.bind(this);
     this.handleNameChange = this.handleNameChange.bind(this);
     this.keyPress = this.keyPress.bind(this);
     this.state = {
-      
-        currentUser: {name: 'Anonymous'}, // optional. if currentUser is not defined, it means the user is Anonymous
-        messages: [],
-        value:'',
-        counter: 0
-      }
-    }
+      currentUser: { name: "Anonymous" },
+      messages: [],
+      value: "",
+      counter: 0
+    };
+  }
 
-handleNameChange(ef) {
-  if (ef.key === 'Enter') {
+  handleNameChange(ef) {
+    if (ef.key === "Enter") {
+      const newName = ef.target.value;
+      const { currentUser } = this.state;
 
-
-    const newName = ef.target.value;
-    const {currentUser} = this.state
-   
-    let newNotification = {
-      'type': 'postNotification',
-      'content': `${currentUser.name} changed their name to ${newName}`,
-      'username': newName,
-      'id': uuidv1()
-    }
-    this.socket.send(JSON.stringify(newNotification))
-      currentUser.name = newName
+      let newNotification = {
+        type: "postNotification",
+        content: `${currentUser.name} changed their name to ${newName}`,
+        username: newName,
+        id: uuidv1()
+      };
+      this.socket.send(JSON.stringify(newNotification));
+      currentUser.name = newName;
     }
   }
-  
-    
 
-  
   // in App.jsx
-componentDidMount() {
-  console.log('componentDidMount <App />');
+  componentDidMount() {
+    console.log("componentDidMount <App />");
 
-  this.socket = new WebSocket("ws://localhost:3001/");
-  this.socket.onopen = () => console.log("connected to server");
-  this.socket.onmessage = (event) => {
-    console.log(event.data)
-    let storeUserData = JSON.parse(event.data)
+    this.socket = new WebSocket("ws://localhost:3001/");
+    this.socket.onopen = () => console.log("connected to server");
+    this.socket.onmessage = event => {
+      console.log(event.data);
+      let storeUserData = JSON.parse(event.data);
 
-    switch(storeUserData.type){
-      case 'UserCounter':
-      // handle incoming message
-      this.setState({counter: storeUserData.userCount})
-        break;
-      case 'incomingMessage':
-      this.setState({curData: storeUserData.newMesage})
-      console.log(this.setState({curData: storeUserData.newMesage.userCounter})
-      )
-      break;
-      default:
-        //show an error in the console if the message type in unknown
-        throw new Error('Unknown event type ' + storeUserData.type);
-    }
-    let curData = this.state.messages;
-    curData.push(storeUserData);
-    this.setState({messages: curData})
+      switch (storeUserData.type) {
+        case "UserCounter":
+          // handle incoming message
+          this.setState({ counter: storeUserData.userCount });
+          break;
+        case "incomingMessage":
+          this.setState({ curData: storeUserData.newMesage });
+          console.log(
+            this.setState({ curData: storeUserData.newMesage })
+          );
+        case 'postNotification':
+          this.setState({newMesage: storeUserData})
+          break;
+        default:
+          //show an error in the console if the message type in unknown
+          throw new Error("Unknown event type " + storeUserData.type);
+      }
+      let curData = this.state.messages;
+      curData.push(storeUserData);
+      this.setState({ messages: curData });
+    };
+
   }
-
-
-
-  setTimeout(() => {
-    console.log('Simulating incoming message');
-    const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
-    const messages = this.state.messages.concat(newMessage)
-    
-    this.setState({messages: messages})
-  }, 3000);
-
-}
-handleChange(e) {
-  this.setState({value: e.target.value});
+  handleChange(e) {
+    this.setState({ value: e.target.value });
   }
-keyPress(e){
-  if(e.key == 'Enter'){
-    let newMesage = {
-      type: 'incomingMessage',
-      content: e.target.value,
-      username: this.state.currentUser.name,
-      id: uuidv1()
-    }
-    this.socket.send(JSON.stringify(newMesage))
+  keyPress(e) {
+    if (e.key == "Enter") {
+      let newMesage = {
+        type: "incomingMessage",
+        content: e.target.value,
+        username: this.state.currentUser.name,
+        id: uuidv1()
+      };
+      this.socket.send(JSON.stringify(newMesage));
     }
   }
-
 
   render() {
     return (
-    <div>
-    <a className="navbar-brand-name">Current Users Online:{this.state.counter}</a>
-    <ChatBar currentUser={this.state.currentUser} keyPress={this.keyPress} handleChange={this.handleChange} value={this.state.value} handleNameChange={this.handleNameChange} />,
-    <MessageList messages={this.state.messages}/>
-    </div>
-      );
+      <div>
+        <a className="navbar-brand-name">
+        {this.state.counter} users online
+        </a>
+        <ChatBar
+          currentUser={this.state.currentUser}
+          keyPress={this.keyPress}
+          handleChange={this.handleChange}
+          value={this.state.value}
+          handleNameChange={this.handleNameChange}
+        />
+        ,
+        <MessageList messages={this.state.messages} />
+      </div>
+    );
   }
 }
 
